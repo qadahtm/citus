2d5d534bd675467a708eafeb0ef1dd7e4e7bed58
Jelte Fennema <github-tech@jeltef.nl>
2019-09-06 14:30:32 +0200

    Simplify not supported errors for shard rebalancer

--- src/backend/distributed/master/shard_rebalancer.c
+++ src/backend/distributed/master/shard_rebalancer.c
@@ -4,7 +4,7 @@
  *
  * Function definitions for the shard rebalancer tool.
  *
- * Copyright (c) 2014, Citus Data, Inc.
+ * Copyright (c) 2019, Citus Data, Inc.
  *
  * $Id$
  *
@@ -13,64 +13,16 @@
 
 #include "postgres.h"
 #include "fmgr.h"
-#include <stdnoreturn.h>
-
-/* declarations for dynamic loading */
-PG_FUNCTION_INFO_V1(rebalance_table_shards);
-PG_FUNCTION_INFO_V1(replicate_table_shards);
-PG_FUNCTION_INFO_V1(get_rebalance_table_shards_plan);
-PG_FUNCTION_INFO_V1(get_rebalance_progress);
-
-static noreturn void
-NotSupportedFunction(char *name)
-{
-	ereport(ERROR, (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-					errmsg("%s() is only supported on Citus Enterprise", name)));
-}
 
 
-/*
- * rebalance_table_shards rebalances the shards across the workers.
- */
-Datum
-rebalance_table_shards(PG_FUNCTION_ARGS)
-{
-	NotSupportedFunction("rebalance_table_shards");
-}
+#define NOT_SUPPORTED_IN_COMMUNITY(name) \
+	PG_FUNCTION_INFO_V1(name); \
+	Datum name(PG_FUNCTION_ARGS) { \
+		ereport(ERROR, (errcode(ERRCODE_FEATURE_NOT_SUPPORTED), \
+						errmsg(# name "() is only supported on Citus Enterprise"))); \
+	}
 
-
-/*
- * replicate_table_shards replicates under-replicated shards of the specified
- * table.
- */
-Datum
-replicate_table_shards(PG_FUNCTION_ARGS)
-{
-	NotSupportedFunction("replicate_table_shards");
-}
-
-
-/*
- * get_rebalance_table_shards_plan function calculates the shard move steps
- * required for the rebalance operations including the ones for colocated
- * tables.
- */
-Datum
-get_rebalance_table_shards_plan(PG_FUNCTION_ARGS)
-{
-	NotSupportedFunction("get_rebalance_table_shards_plan");
-}
-
-
-/*
- * get_rebalance_progress collects information about the ongoing rebalance operations and
- * returns the concatenated list of steps involved in the operations, along with their
- * progress information. Currently the progress field can take 4 integer values
- * (-1: error, 0: waiting, 1: moving, 2: moved). The progress field is of type bigint
- * because we may implement a more granular, byte-level progress as a future improvement.
- */
-Datum
-get_rebalance_progress(PG_FUNCTION_ARGS)
-{
-	NotSupportedFunction("get_rebalance_progress");
-}
+NOT_SUPPORTED_IN_COMMUNITY(rebalance_table_shards);
+NOT_SUPPORTED_IN_COMMUNITY(replicate_table_shards);
+NOT_SUPPORTED_IN_COMMUNITY(get_rebalance_table_shards_plan);
+NOT_SUPPORTED_IN_COMMUNITY(get_rebalance_progress);
